c43c516fc40187af6ab24014ca8d5f5ebf65bf2f
Sam <swemoulson@gmail.com>
2020-08-17 21:12:09 +1200

    database workin

--- run.py
+++ run.py
@@ -9,36 +9,46 @@ muscle_group = ['Legs','Arms', 'Core']
 length = ['10mins', '15mins', '20mins']
 difficulty = ['Easy', 'Medium', 'Hard']
 '''
-@app.route('/')
-def index():
-
-
-
-    db = sqlite3.connect(HIITDB)
+def fetchWorkout(db):
 
     muscle_group = []
-    length = []
-    difficulty = []
-
     cur = db.execute('SELECT muscles FROM muscle_group')
     for row in cur:
         muscle_group.append(list(row))
 
+    length = []
     cur = db.execute('SELECT length FROM length')
     for row in cur:
         length.append(list(row))
 
+    difficulty = []
     cur = db.execute('SELECT difficulty FROM difficulty')
     for row in cur:
         difficulty.append(list(row))
-    db.close()
-    
 
-    return render_template('index.html', disclaimer='no equipment required', muscles=muscle_group, length=length,
-    difficulty=difficulty)
+
+    return {'muscle_group':muscle_group, 'length':length, 'difficulty':difficulty}
+
+@app.route('/')
+def index():
+
+
+    db = sqlite3.connect(HIITDB)
+    workout = fetchWorkout(db)
+    db.close()
+
+    return render_template('index.html',
+    disclaimer='no equipment required',
+    muscles=workout['muscle_group'],
+    length=workout['length'],
+    difficulty=workout['difficulty']
+    )
 
 
 @app.route('/result', methods=['POST'])
 def result():
+    db = sqlite3.connect(HIITDB)
+    workout = fetchWorkout(db)
+    db.close()
     print(request.form)
     return render_template('result.html')
